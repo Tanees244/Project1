d093ad283ef5c7db8468d84a9b0d4a2f2cc20207
Tanees <k201879@nu.edu.pk>
2023-12-01 00:26:29 +0500

    commit file name changed

--- src/components/.test.js
+++ src/components/.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import HomePage from '../components/HomePage';
-import CertificationPage from '../components/CertificationPage';
-import EducationPage from '../components/EducationPage';
-import ProjectsPage from '../components/ProjectsPage';
+import HomePage from './HomePage';
+import CertificationPage from './CertificationPage';
+import EducationPage from './EducationPage';
+import ProjectsPage from './ProjectsPage';
 
 describe('HomePage Component', () => {
   it('renders the component without crashing', () => {
